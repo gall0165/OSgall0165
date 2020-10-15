63f7252fa1ee90049058e814582da18104ab8ac7
Yaciel Gallegos <gall0165@flinders.edu.au>
2020-10-15 20:05:40 +1030

    Initial Test checkpoints Unit 5

---
+++ unit5/os5.1.asm
@@ -0,0 +1,958 @@
+// XMega65 Kernal Development Template
+// Each function of the kernal is a no-args function
+// The functions are placed in the SYSCALLS table surrounded by JMP and NOP
+  .file [name="os5.1.bin", type="bin", segments="XMega65Bin"]
+.segmentdef XMega65Bin [segments="Syscall, Code, Data, Stack, Zeropage"]
+.segmentdef Syscall [start=$8000, max=$81ff]
+.segmentdef Code [start=$8200, min=$8200, max=$bdff]
+.segmentdef Data [startAfter="Code", min=$8200, max=$bdff]
+.segmentdef Stack [min=$be00, max=$beff, fill]
+.segmentdef Zeropage [min=$bf00, max=$bfff, fill]
+  .const SIZEOF_WORD = 2
+  .const OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_PROCESS_NAME = 2
+  .const OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS = 4
+  .const OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_END_ADDRESS = 8
+  .const OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORED_STATE = $c
+  .label RASTER = $d012
+  .label VIC_MEMORY = $d018
+  .label SCREEN = $400
+  .label BGCOL = $d021
+  .label COLS = $d800
+  .const BLUE = 6
+  .const WHITE = 1
+  // Process stored state will live at $C000-$C7FF, with 256 bytes
+  // for each process reserved
+  .label stored_pdbs = $c000
+  //To save writing 0x4C and 0xEA all the time, we define them as constants 
+  .const JMP = $4c
+  .const NOP = $ea
+  .label current_screen_line = 8
+  .label current_screen_x = $a
+  lda #<SCREEN
+  sta.z current_screen_line
+  lda #>SCREEN
+  sta.z current_screen_line+1
+  lda #0
+  sta.z current_screen_x
+  jsr main
+  rts
+.segment Code
+main: {
+    jsr exit_hypervisor
+    rts
+}
+/*
+XXX - Copy in your syscalls, RESET() function and entry point tables from
+      checkpoint3.1
+
+XXX - Now add the following lines to your RESET() function just before the
+      "// Loop forever with 2 bars"
+    current_screen_line = SCREEN;
+    print_newline();
+    print_newline();
+    print_newline();
+
+    describe_pdb(0);
+*/
+exit_hypervisor: {
+    //Trigger exit from Hypervisor mode
+    lda #1
+    sta $d67f
+    rts
+}
+CPUKIL: {
+    jsr exit_hypervisor
+    rts
+}
+undefined_trap: {
+    jsr exit_hypervisor
+    rts
+}
+VF011WR: {
+    jsr exit_hypervisor
+    rts
+}
+VF011RD: {
+    jsr exit_hypervisor
+    rts
+}
+ALTTABKEY: {
+    jsr exit_hypervisor
+    rts
+}
+RESTORKEY: {
+    jsr exit_hypervisor
+    rts
+}
+PAGFAULT: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3F: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3E: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3D: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3C: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3B: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL3A: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL39: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL38: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL37: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL36: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL35: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL34: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL33: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL32: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL31: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL30: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2F: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2E: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2D: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2C: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2B: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL2A: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL29: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL28: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL27: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL26: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL25: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL24: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL23: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL22: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL21: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL20: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1F: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1E: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1D: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1C: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1B: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL1A: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL19: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL18: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL17: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL16: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL15: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL14: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL13: {
+    jsr exit_hypervisor
+    rts
+}
+SECUREXIT: {
+    jsr exit_hypervisor
+    rts
+}
+SECURENTR: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL10: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0F: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0E: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0D: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0C: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0B: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL0A: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL09: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL08: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL07: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL06: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL05: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL04: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL03: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL02: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL01: {
+    jsr exit_hypervisor
+    rts
+}
+SYSCALL00: {
+    jsr exit_hypervisor
+    rts
+}
+RESET: {
+    .label sc = $d
+    .label msg = 2
+    lda #$14
+    sta VIC_MEMORY
+    ldx #' '
+    lda #<SCREEN
+    sta.z memset.str
+    lda #>SCREEN
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    ldx #WHITE
+    lda #<COLS
+    sta.z memset.str
+    lda #>COLS
+    sta.z memset.str+1
+    lda #<$28*$19
+    sta.z memset.num
+    lda #>$28*$19
+    sta.z memset.num+1
+    jsr memset
+    lda #<SCREEN
+    sta.z current_screen_line
+    lda #>SCREEN
+    sta.z current_screen_line+1
+    lda #<SCREEN+$28
+    sta.z sc
+    lda #>SCREEN+$28
+    sta.z sc+1
+    lda #<MESSAGE
+    sta.z msg
+    lda #>MESSAGE
+    sta.z msg+1
+  __b1:
+    ldy #0
+    lda (msg),y
+    cmp #0
+    bne __b2
+    jsr print_newline
+    jsr print_newline
+    jsr print_newline
+    jsr describe_pdb
+  __b4:
+    lda #$36
+    cmp RASTER
+    beq __b5
+    lda #$42
+    cmp RASTER
+    beq __b5
+    lda #BLUE
+    sta BGCOL
+    jmp __b4
+  __b5:
+    lda #WHITE
+    sta BGCOL
+    jmp __b4
+  __b2:
+    ldy #0
+    lda (msg),y
+    sta (sc),y
+    inc.z sc
+    bne !+
+    inc.z sc+1
+  !:
+    inc.z msg
+    bne !+
+    inc.z msg+1
+  !:
+    jmp __b1
+}
+describe_pdb: {
+    .label p = stored_pdbs
+    .label n = $d
+    .label ss = 2
+    lda #<message
+    sta.z print_to_screen.c
+    lda #>message
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda #<0
+    sta.z print_hex.value
+    sta.z print_hex.value+1
+    jsr print_hex
+    lda #<message1
+    sta.z print_to_screen.c
+    lda #>message1
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    jsr print_newline
+    lda #<message2
+    sta.z print_to_screen.c
+    lda #>message2
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda p
+    sta.z print_hex.value
+    lda #0
+    sta.z print_hex.value+1
+    jsr print_hex
+    jsr print_newline
+    lda #<message3
+    sta.z print_to_screen.c
+    lda #>message3
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_PROCESS_NAME
+    sta.z n
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_PROCESS_NAME+1
+    sta.z n+1
+    ldx #0
+  __b1:
+    txa
+    tay
+    lda (n),y
+    cmp #0
+    beq __b3
+    cpx #$11
+    bcc __b2
+  __b3:
+    jsr print_newline
+    lda #<message4
+    sta.z print_to_screen.c
+    lda #>message4
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS
+    sta.z print_dhex.value
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+1
+    sta.z print_dhex.value+1
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+2
+    sta.z print_dhex.value+2
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_START_ADDRESS+3
+    sta.z print_dhex.value+3
+    jsr print_dhex
+    jsr print_newline
+    lda #<message5
+    sta.z print_to_screen.c
+    lda #>message5
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_END_ADDRESS
+    sta.z print_dhex.value
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_END_ADDRESS+1
+    sta.z print_dhex.value+1
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_END_ADDRESS+2
+    sta.z print_dhex.value+2
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORAGE_END_ADDRESS+3
+    sta.z print_dhex.value+3
+    jsr print_dhex
+    jsr print_newline
+    lda #<message6
+    sta.z print_to_screen.c
+    lda #>message6
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORED_STATE
+    sta.z ss
+    lda p+OFFSET_STRUCT_PROCESS_DESCRIPTOR_BLOCK_STORED_STATE+1
+    sta.z ss+1
+    ldy #4*SIZEOF_WORD
+    lda (print_hex.value),y
+    pha
+    iny
+    lda (print_hex.value),y
+    sta.z print_hex.value+1
+    pla
+    sta.z print_hex.value
+    jsr print_hex
+    jsr print_newline
+    rts
+  __b2:
+    txa
+    tay
+    lda (n),y
+    jsr print_char
+    inx
+    jmp __b1
+  .segment Data
+    message: .text "pdb#"
+    .byte 0
+    message1: .text ":"
+    .byte 0
+    message2: .text "  pid:          "
+    .byte 0
+    message3: .text "  process name: "
+    .byte 0
+    message4: .text "  mem start:    $"
+    .byte 0
+    message5: .text "  mem end:      $"
+    .byte 0
+    message6: .text "  pc:           $"
+    .byte 0
+}
+.segment Code
+// print_char(byte register(A) c)
+print_char: {
+    ldy.z current_screen_x
+    sta (current_screen_line),y
+    inc.z current_screen_x
+    rts
+}
+print_newline: {
+    lda #$28
+    clc
+    adc.z current_screen_line
+    sta.z current_screen_line
+    bcc !+
+    inc.z current_screen_line+1
+  !:
+    lda #0
+    sta.z current_screen_x
+    rts
+}
+// print_hex(word zeropage(2) value)
+print_hex: {
+    .label __3 = $b
+    .label __6 = $d
+    .label value = 2
+    ldx #0
+  __b1:
+    cpx #8
+    bcc __b2
+    lda #0
+    sta hex+4
+    lda #<hex
+    sta.z print_to_screen.c
+    lda #>hex
+    sta.z print_to_screen.c+1
+    jsr print_to_screen
+    rts
+  __b2:
+    lda.z value+1
+    cmp #>$a000
+    bcc __b4
+    bne !+
+    lda.z value
+    cmp #<$a000
+    bcc __b4
+  !:
+    ldy #$c
+    lda.z value
+    sta.z __3
+    lda.z value+1
+    sta.z __3+1
+    cpy #0
+    beq !e+
+  !:
+    lsr.z __3+1
+    ror.z __3
+    dey
+    bne !-
+  !e:
+    lda.z __3
+    sec
+    sbc #9
+    sta hex,x
+  __b5:
+    asl.z value
+    rol.z value+1
+    asl.z value
+    rol.z value+1
+    asl.z value
+    rol.z value+1
+    asl.z value
+    rol.z value+1
+    inx
+    jmp __b1
+  __b4:
+    ldy #$c
+    lda.z value
+    sta.z __6
+    lda.z value+1
+    sta.z __6+1
+    cpy #0
+    beq !e+
+  !:
+    lsr.z __6+1
+    ror.z __6
+    dey
+    bne !-
+  !e:
+    lda.z __6
+    clc
+    adc #'0'
+    sta hex,x
+    jmp __b5
+  .segment Data
+    hex: .fill 5, 0
+}
+.segment Code
+print_to_screen: {
+    .label c = $d
+  __b1:
+    ldy #0
+    lda (c),y
+    cmp #0
+    bne __b2
+    rts
+  __b2:
+    ldy #0
+    lda (c),y
+    ldy.z current_screen_x
+    sta (current_screen_line),y
+    inc.z current_screen_x
+    inc.z c
+    bne !+
+    inc.z c+1
+  !:
+    jmp __b1
+}
+// print_dhex(dword zeropage(4) value)
+print_dhex: {
+    .label __0 = $f
+    .label value = 4
+    lda #0
+    sta.z __0+2
+    sta.z __0+3
+    lda.z value+3
+    sta.z __0+1
+    lda.z value+2
+    sta.z __0
+    sta.z print_hex.value
+    lda.z __0+1
+    sta.z print_hex.value+1
+    jsr print_hex
+    lda.z value
+    sta.z print_hex.value
+    lda.z value+1
+    sta.z print_hex.value+1
+    jsr print_hex
+    rts
+}
+// Copies the character c (an unsigned char) to the first num characters of the object pointed to by the argument str.
+// memset(void* zeropage($b) str, byte register(X) c, word zeropage($d) num)
+memset: {
+    .label end = $d
+    .label dst = $b
+    .label num = $d
+    .label str = $b
+    lda.z num
+    bne !+
+    lda.z num+1
+    beq __breturn
+  !:
+    lda.z end
+    clc
+    adc.z str
+    sta.z end
+    lda.z end+1
+    adc.z str+1
+    sta.z end+1
+  __b2:
+    lda.z dst+1
+    cmp.z end+1
+    bne __b3
+    lda.z dst
+    cmp.z end
+    bne __b3
+  __breturn:
+    rts
+  __b3:
+    txa
+    ldy #0
+    sta (dst),y
+    inc.z dst
+    bne !+
+    inc.z dst+1
+  !:
+    jmp __b2
+}
+.segment Data
+  MESSAGE: .text "checkpoint 5.1 gall0165"
+  .byte 0
+.segment Syscall
+  //Now we can have a nice table of up to 64 SYSCALL handlers expressed
+  //in fairly readeable and easy format.
+  //Each line is an instance of the struct Syscall from above, with the JMP
+  //opcode value, the address of the handler routine and the NOP opcode value.
+  SYSCALLS: .byte JMP
+  .word SYSCALL00
+  .byte NOP, JMP
+  .word SYSCALL01
+  .byte NOP, JMP
+  .word SYSCALL02
+  .byte NOP, JMP
+  .word SYSCALL03
+  .byte NOP, JMP
+  .word SYSCALL04
+  .byte NOP, JMP
+  .word SYSCALL05
+  .byte NOP, JMP
+  .word SYSCALL06
+  .byte NOP, JMP
+  .word SYSCALL07
+  .byte NOP, JMP
+  .word SYSCALL08
+  .byte NOP, JMP
+  .word SYSCALL09
+  .byte NOP, JMP
+  .word SYSCALL0A
+  .byte NOP, JMP
+  .word SYSCALL0B
+  .byte NOP, JMP
+  .word SYSCALL0C
+  .byte NOP, JMP
+  .word SYSCALL0D
+  .byte NOP, JMP
+  .word SYSCALL0E
+  .byte NOP, JMP
+  .word SYSCALL0F
+  .byte NOP, JMP
+  .word SYSCALL10
+  .byte NOP, JMP
+  .word SECURENTR
+  .byte NOP, JMP
+  .word SECUREXIT
+  .byte NOP, JMP
+  .word SYSCALL13
+  .byte NOP, JMP
+  .word SYSCALL14
+  .byte NOP, JMP
+  .word SYSCALL15
+  .byte NOP, JMP
+  .word SYSCALL16
+  .byte NOP, JMP
+  .word SYSCALL17
+  .byte NOP, JMP
+  .word SYSCALL18
+  .byte NOP, JMP
+  .word SYSCALL19
+  .byte NOP, JMP
+  .word SYSCALL1A
+  .byte NOP, JMP
+  .word SYSCALL1B
+  .byte NOP, JMP
+  .word SYSCALL1C
+  .byte NOP, JMP
+  .word SYSCALL1D
+  .byte NOP, JMP
+  .word SYSCALL1E
+  .byte NOP, JMP
+  .word SYSCALL1F
+  .byte NOP, JMP
+  .word SYSCALL20
+  .byte NOP, JMP
+  .word SYSCALL21
+  .byte NOP, JMP
+  .word SYSCALL22
+  .byte NOP, JMP
+  .word SYSCALL23
+  .byte NOP, JMP
+  .word SYSCALL24
+  .byte NOP, JMP
+  .word SYSCALL25
+  .byte NOP, JMP
+  .word SYSCALL26
+  .byte NOP, JMP
+  .word SYSCALL27
+  .byte NOP, JMP
+  .word SYSCALL28
+  .byte NOP, JMP
+  .word SYSCALL29
+  .byte NOP, JMP
+  .word SYSCALL2A
+  .byte NOP, JMP
+  .word SYSCALL2B
+  .byte NOP, JMP
+  .word SYSCALL2C
+  .byte NOP, JMP
+  .word SYSCALL2D
+  .byte NOP, JMP
+  .word SYSCALL2E
+  .byte NOP, JMP
+  .word SYSCALL2F
+  .byte NOP, JMP
+  .word SYSCALL30
+  .byte NOP, JMP
+  .word SYSCALL31
+  .byte NOP, JMP
+  .word SYSCALL32
+  .byte NOP, JMP
+  .word SYSCALL33
+  .byte NOP, JMP
+  .word SYSCALL34
+  .byte NOP, JMP
+  .word SYSCALL35
+  .byte NOP, JMP
+  .word SYSCALL36
+  .byte NOP, JMP
+  .word SYSCALL37
+  .byte NOP, JMP
+  .word SYSCALL38
+  .byte NOP, JMP
+  .word SYSCALL39
+  .byte NOP, JMP
+  .word SYSCALL3A
+  .byte NOP, JMP
+  .word SYSCALL3B
+  .byte NOP, JMP
+  .word SYSCALL3C
+  .byte NOP, JMP
+  .word SYSCALL3D
+  .byte NOP, JMP
+  .word SYSCALL3E
+  .byte NOP, JMP
+  .word SYSCALL3F
+  .byte NOP
+  //In this example we had only two SYSCALLs defined, so rather than having
+  //another 62 lines, we can just ask KickC to make the TRAP table begin
+  //at the next multiple of $100, i.e., at $8100.
+  .align $100
+  TRAPS: .byte JMP
+  .word RESET
+  .byte NOP, JMP
+  .word PAGFAULT
+  .byte NOP, JMP
+  .word RESTORKEY
+  .byte NOP, JMP
+  .word ALTTABKEY
+  .byte NOP, JMP
+  .word VF011RD
+  .byte NOP, JMP
+  .word VF011WR
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word undefined_trap
+  .byte NOP, JMP
+  .word CPUKIL
+  .byte NOP
